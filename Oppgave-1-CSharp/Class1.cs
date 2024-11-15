6f83474f95967d0f074597c8af28ed16a1e69564
DanielLarsen-spec <larsen97daniel@gmail.com>
2024-11-15 14:17:54 +0100

    Initial commit

---
+++ Oppgave-1-CSharp/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oppgave_1_CSharp
+{
+    internal class Class1
+    {
+    }
+}
